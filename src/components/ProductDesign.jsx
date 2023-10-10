9e3844a6316b8bba15db7fc760ef2f524baa774c
umut-enes-akkoyun <akkoyunumutenes@gmail.com>
2023-10-10 22:40:35 +0300

    paragraf ui fixed

--- src/components/ProductDesign.jsx
+++ src/components/ProductDesign.jsx
@@ -4,15 +4,15 @@ import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
 
 const Desc = styled.div`
-  width: 200px;
-  height: 70px;
+  width: 360px;
   padding: 20px;
   background-color: white;
   border-radius: 10px;
   position: absolute;
   bottom: 200px;
   right: 100px;
-
+  font-size: 20px;
+  font-family: "Montserrat", sans-serif;
   @media only screen and (max-width: 768px) {
     top: 0;
     bottom: 0;
@@ -34,7 +34,14 @@ const ProductDesign = () => {
         </Suspense>
       </Canvas>
       <Desc>
-        Traditional centralized optimization and management schemes may be incompatible with a changing energy system whose structure is becoming increasingly distributed. This challenge can hopefully be addressed by blockchain. However, existing blockchains have not been well prepared to integrate mathematical optimization, which plays a key role in many energy system applications. Here we propose a blockchain consensus mechanism tailored to support mathematical optimization problems, called Proof of Solution (PoSo). PoSo mimics Proof of Work (PoW) by replacing the meaningless mathematical puzzle in PoW with a meaningful optimization problem. This is inspired by the fact that both the solutions to the puzzle and to an optimization problem are hard to find but easy to verify. We show the security and necessity of PoSo by using PoSo to enable energy dispatch and trading for two integrated energy systems. The results show that compared with existing optimization schemes, PoSo ensures that only the optimal solution is accepted and executed by participants. Further, compared with existing blockchains, PoSo can seamlessly incorporate mathematical optimization and minimize the workload associated with searching and verifying the optimum.
+        Sittaris generates the energy used for Bitcoin mining with solar panels that are place on top of greenhouses. Trio Investment BV is an investment
+         company that invests in sustainable energy in Europe and emerging markets. With Sittaris they offer investment opportunities with returns in bitcoin and euros.
+
+        Sittaris uses blockchain technology to introduce a trustless framework known as "Proof of Energy" (“POE”) for transparently verifying the source 
+        and quantity of energy generated. Energy generated in our solar parks is sold back to the grid or channeled into a green-mining setup to generate Bitcoin.
+         At the core of our operations, we not only achieve zero emissions but also zero waste through the efficient use of excess mining-generated heat in our greenhouses.
+
+
       </Desc>
     </>
   );
