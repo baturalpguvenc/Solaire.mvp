bbdaf3afed54cd347706850ae140a737707c4da2
Baturalp Güvenç <cloudmelon74@gmail.com>
2023-10-11 15:46:56 +0300

    texture

--- src/sections/TimeLine.jsx
+++ src/sections/TimeLine.jsx
@@ -11,131 +11,93 @@ import Mining from "../assets/images/birmine.jpeg";
 
 const Section = styled.div`
   height: 100vh;
-  height: 100svh;
   scroll-snap-type: y proximity;
   scroll-snap-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
-  align-content: center;
   justify-content: center;
-`;
-
-const TimelineWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  align-content: center;
-  justify-content: center;
-
-  width: 100%;
-`;
-
-const Event = styled.div`
-  position: relative;
-  width: 50%;
-  background: #3498db;
-  padding: 10px;
-  border-radius: 5px;
-  margin: 10px 0;
-  text-align: center;
-  color: #fff;
-  font-size: 14px !important; /* Yazı fontunu küçült */
-`;
-
-const Date = styled.div`
-  position: absolute;
-  top: -20px;
-  left: 50%;
-  transform: translateX(-50%);
-  background: #333;
-  color: #fff;
-  padding: 5px 10px;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  font-size: 12px !important; /* Yazı fontunu küçült */
 `;
 
 const Container = styled.div`
   height: 100%;
   scroll-snap-align: center;
-  width: 1400px;
+  width: 70%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
 
-  @media only screen and (max-width: 768px) {
+  @media only screen and (min-width: 768px) {
     width: 100%;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    flex-direction: row;
   }
 `;
 
+const Event = styled.div`
+  width: 70%;
+  padding: 20px;
+  border-radius: 5px;
+  margin: 10px;
+  text-align: center;
+  color: #fff;
+  font-size: 30px !important;
+  font-weight: bold;
+  font-family: Montserrat, sans-serif !important;
+  background: linear-gradient(to right, #22c1c3, #fdbb2d);
+
+  @media only screen and (min-width: 768px) {
+    width: 80%;
+  }
+`;
+
+const Date = styled.div`
+  width: 70%;
+  padding: 20px;
+  font-size: 36px !important; /* Yazıları daha büyük yapmak için font-size'i artırdık */
+  font-weight: bold;
+  font-family: Montserrat, sans-serif !important;
+  background: linear-gradient(to right, #fd711d, #fdbb2d);
+  color: #fff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  margin: 10px;
+
+  @media only screen and (min-width: 768px) {
+    width: 70%;
+  }
+`;
+
+
 export default function TimelineComponent() {
   const events = [
-    { date: "Q4 2023", description: "The first two solar parks that will be equiped with bitcoin mining faclilities are already selected. " },
-    { date: "Q4 2023", description: " They are located in the Netherlands and will become active in Q4 of 2023." },
-    { date: "Q1 2024", description: "Marketing" },
-    { date: "Q4 2024", description: "Launch" },
+    { date: "Q4 2023", description: "The first two solar parks that will be equipped with bitcoin mining facilities are already selected." },
+    { date: "Q4 2023", description: "They are located in the Netherlands and will become active in Q4 of 2023." },
+    { date: "Q1 2024", description: "The expansion of the Sittaris services - energy storage, grid connection and heat production is anticipated for Q1 - 2024." },
+    { date: "Q2 2024", description: "Launch" },
   ];
 
   return (
     <Section>
       <Container>
-        <div style={{ display: "flex", alignItems: "center", justifyContent:"center" }}>
-          <div id={"timeline"} />
-          <img src={Mining} style={{ height: "200px", objectFit: "cover" }} />
-          <div
-            style={{
-              width: "600px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignContent: "center",
-            }}
-          >
-            {events.map((event, i) => {
-  return (
-    <TimelineItem key={i}>
-      <TimelineOppositeContent
-        style={{ fontFamily: "SatoshiMedium, sans-serif", fontSize: "10px !important" }} // Metin fontunu burada güncelle
-        color="text.secondary"
-      >
-        <p>{event.description}</p>
-      </TimelineOppositeContent>
-
-      <TimelineSeparator
-        style={{ marginTop: "30px !important", marginBottom: "-30px !important" }}
-      >
-        <div
-          style={{
-            width: "10px !important",
-            height: "10px !important",
-            backgroundColor: "white",
-            borderRadius: "50%",
-          }}
-        />
-        {events.length - 1 !== i && (
-          <div
-            style={{
-              width: "1px",
-              height: "100%",
-              backgroundColor: "darkGreen",
-            }}
-          />
-        )}
-      </TimelineSeparator>
-
-      <TimelineContent
-        style={{ fontFamily: "SatoshiMedium, sans-serif", fontSize: "20px !important" }} // Metin fontunu burada güncellemeye çalıştım
-      >
-        <p>{event.date}</p>
-      </TimelineContent>
-    </TimelineItem>
-  );
-})}
-          </div>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <Timeline align="left">
+            {events.map((event, i) => (
+              <TimelineItem key={i}>
+                <TimelineOppositeContent>
+                  <Event>{event.description}</Event>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  {i < events.length - 1 && <TimelineConnector />}
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Date>{event.date}</Date>
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Timeline>
         </div>
       </Container>
     </Section>
